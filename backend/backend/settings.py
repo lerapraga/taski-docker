bf2f97230d22087d4cf11186832841455f7ad159
Lera Praga <49233390+lerapraga@users.noreply.github.com>
2023-07-08 13:11:13 +0500

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['84.201.162.140', '127.0.0.1', 'localhost','lera.hopto.org']
+ALLOWED_HOSTS = ['84.201.162.140', '127.0.0.1', 'localhost', 'lera.hopto.org']
 # os.getenv('ALLOWED_HOSTS').split(',')
 
 
